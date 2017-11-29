9e1d2e08e4e69b2541a52cafb46f545fa1fa1215
Erin Leck <erinleck@gmail.com>
2017-11-29 21:16:32 +0000

    get_mentor_availability fix

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,8 +21,12 @@ class Kele
     JSON.parse(response.body)
   end
   
-  def get_mentor_availability
-    response = self.class.get(@base_url + '/mentors/946/student_availability', headers: {"authorization" => @auth_token})
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(@base_url + "/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
     JSON.parse(response.body)
+  end
+  
+  def get_roadmap
+    
   end
 end
