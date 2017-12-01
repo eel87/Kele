82dfe7a613ae1fc85bed1d5169f5cb08135c5848
Erin Leck <erinleck@gmail.com>
2017-12-01 19:05:57 +0000

    create_submission method created

--- lib/kele.rb
+++ lib/kele.rb
@@ -49,4 +49,18 @@ class Kele
     response = self.class.post(@base_url + "/messages", values)
     puts response.body
   end
+  
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    values = {
+      headers: {"authorization" => @auth_token},
+      body: { "checkpoint_id" => checkpoint_id,
+              "assignment_branch" => assignment_branch,
+              "assignment_commit_link" => assignment_commit_link,
+              "comment" => comment,
+              "enrollment_id" => enrollment_id
+      }
+    }
+    response = self.class.post(@base_url + "/checkpoint_submissions", values)
+    puts response.body
+  end
 end
